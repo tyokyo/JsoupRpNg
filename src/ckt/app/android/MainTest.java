9831a146b2b9c745640265318e4477beb9f56de9
qiang.zhang <qiang.zhang@ck-telecom.com>
2016-12-30 10:21:40 +0800

    second

--- src/ckt/app/android/MainTest.java
+++ src/ckt/app/android/MainTest.java
@@ -1,24 +1,35 @@
 package ckt.app.android;
 
+import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.Test;
 
 public class MainTest {
-  @Test
-  public void f1() {
-	  System.out.println("test one");
-  }
-  @Test
-  public void f2() {
-	  System.out.println("test two");
-  }
-  @BeforeTest
-  public void beforeTest() {
-  }
+	@Test(description ="test one")
+	public void af() {
+		System.out.println("test one");
+	}
+	@Test(description ="test two")
+	public void cf2() {
+		System.out.println("test two");
+	}
+	@Test(description ="test three")
+	public void df3() {
+		System.out.println("test three");
+		Assert.assertEquals(true, false);
+	}
+	@Test(description ="test four")
+	public void bf4() {
+		System.out.println("test four");
+		Assert.assertEquals(true, true);
+	}
+	@BeforeTest
+	public void beforeTest() {
+	}
 
-  @AfterTest
-  public void afterTest() {
-  }
+	@AfterTest
+	public void afterTest() {
+	}
 
 }
