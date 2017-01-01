7ad651f5cac21a9331ac05cdb23bd239c9fbce07
qiang.zhang <qiang.zhang@ck-telecom.com>
2017-01-02 00:18:42 +0800

    add renamed dir

---
+++ src/web/testcase/AccountTest.java
@@ -0,0 +1,29 @@
+package web.testcase;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.openqa.selenium.WebDriver.Navigation;
+import org.testng.annotations.AfterClass;
+
+import web.base.Constant;
+import web.util.VP;
+
+public class AccountTest extends VP{
+	@Test
+	public void LoginManagementAccount() {
+		Navigation navigation = driver.navigate();
+		navigation.to(Constant.url_management);
+		clickById("UserName");
+		clickById("Password");
+		clickByCssSelector("btn green-turquoise btn-block bold uppercase");
+	}
+	@BeforeClass
+	public void beforeClass() {
+
+	}
+
+	@AfterClass
+	public void afterClass() {
+		driver.close();
+	}
+}
