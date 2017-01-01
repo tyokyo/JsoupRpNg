227c460d0e5b1f7088c5b02a16fb6b6444b53bae
qiang.zhang <qiang.zhang@ck-telecom.com>
2017-01-01 23:40:09 +0800

    renamed from ReportLogger file

--- src/org/uncommons/reportng/Reporters.java
+++ src/org/uncommons/reportng/Reporters.java
@@ -1,27 +1,32 @@
 package org.uncommons.reportng;
 
 import org.testng.Reporter;
+import org.testng.log4testng.Logger;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 /** Simple logger interface. */
-public class ReportLogger {
+public class Reporters {
+  private static Logger logger = Logger.getLogger(Reporters.class);
   private static final ThreadLocal<DateFormat> DATE_FORMAT = new ThreadLocal<DateFormat>() {
     @Override protected DateFormat initialValue() {
       return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
     }
   };
   public static void logError(String message, Object... args) {
+    logger.info(getPrefix() + String.format(message, args));
     Reporter.log(getPrefix() + String.format(message, args));
   }
 
   public static void logInfo(String message, Object... args) {
+    logger.info(getPrefix() + String.format(message, args));
     Reporter.log(getPrefix() + String.format(message, args));
   }
 
   public static void logDebug(boolean debug, String message, Object... args) {
+    logger.info(getPrefix() + String.format(message, args));
     if (debug) Reporter.log(getPrefix() + String.format(message, args));
   }
 
@@ -34,4 +39,5 @@ public class ReportLogger {
     String timestamp = DATE_FORMAT.get().format(new Date());
     return String.format("%s [%s.%s] ", timestamp, className, methodName);
   }
+
 }
